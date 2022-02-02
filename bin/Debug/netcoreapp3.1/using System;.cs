d2f4296bca784ba2ff29e64ce15f5347e3e8c283
Carlos <crealmadrid618@gmail.com>
2022-02-02 13:17:52 +0100

    empezamos proyecto

---
+++ bin/Debug/netcoreapp3.1/using System;.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Mi_primer_proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        int num1 = 1;
+        int num2 = 2; 
+        int resultado = num1+num2;
+        
+
+        bool resultado = num1.equals(num2);
+        Console.WriteLine(resultado.toint);
+        
+        If num1=num2; 
+        Console.WriteLine("El" + num1 "y" "el" num2 son iguales);
+        Console.WriteLine("Verdadero");
+
+        else 
+        Console.WriteLine("Falso");
+
+
+
